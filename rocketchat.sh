80dca20d17ed40a2a6e261cd130cd7385e47b444
Nicholas Chung <nich.chung@gmail.com>
2017-02-07 19:25:11 -0500

    syntax and formatting

--- rocketchat.sh
+++ rocketchat.sh
@@ -3,31 +3,51 @@
 # rocketchat installation script for Centos/RHEL 7
 # author: nicholas chung
 # date: 12.8.16
-# version 0.1
+# version 0.2
+# instructions:
+# 1. change root URLs
+# 2. make script executable
+# 3. run the script
+# TODO: make interactive, possibly with Python
 ######################################################
-sudo yum -y update # system update
-sudo yum -y install epel-release vim && sudo yum -y update # add extra packages and update
-touch /etc/yum.repos.d/mongodb-org-3.2.repo # add mongodb repo with config below
-cat > /etc/yum.repos.d/mongodb-org-3.2.repo << EOL
+# system update
+yum -y update # system update
+# add extra packages and update
+yum -y install epel-release vim && yum -y update
+
+# add mongodb repo with config below
+touch /etc/yum.repos.d/mongodb-org-3.2.repo
+cat <<EOT >> /etc/yum.repos.d/mongodb-org-3.2.repo
 [mongodb-org-3.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-3.2.asc
-EOL # end repo config
-sudo yum install -y nodejs httpd curl GraphicsMagick npm mongodb-org-server mongodb-org gcc-c++ # install Rocket.Chat dependencies
-sudo chkconfig mongod on
-sudo npm install -g inherits # install node dependencies
-sudo npm install -g n
-sudo n 4.5 # install node version 4.5
-cd /opt # download Rocket.Chat files here
-sudo curl -L https://rocket.chat/releases/latest/download -o rocket.chat.tgz
-sudo tar -zxvf rocket.chat.tgz
-sudo mv bundle Rocket.Chat
+EOT
+# end repo config
+
+# install Rocket.Chat dependencies
+yum install -y nodejs httpd curl GraphicsMagick npm mongodb-org-server mongodb-org gcc-c++
+chkconfig mongod on
+
+# install node dependencies
+npm install -g inherits # install node dependencies
+npm install -g n
+
+# install node version 4.5
+n 4.5
+
+# download Rocket.Chat files here
+cd /opt
+curl -L https://rocket.chat/releases/latest/download -o rocket.chat.tgz
+tar -zxvf rocket.chat.tgz
+mv bundle Rocket.Chat
 cd Rocket.Chat/programs/server
-sudo npm install
-cat > /usr/lib/systemd/system/rocketchat.service << EOL
+npm install
+
+# Add to systemd
+cat <<EOT >> /usr/lib/systemd/system/rocketchat.service
 [Unit]
 Description=The Rocket.Chat server
 After=network.target remote-fs.target nss-lookup.target httpd.target mongod.target
@@ -40,13 +60,16 @@ User=root
 Environment=MONGO_URL=mongodb://localhost:27017/rocketchat ROOT_URL=http://192.168.100.239:3000/ PORT=3000 # change IP address
 [Install]
 WantedBy=multi-user.target
-EOL # end systemd config
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo systemctl start mongod.service
-sudo systemctl enable mongod.service
-sudo systemctl start rocketchat.service
-sudo systemctl enable rocketchat.service
+EOT
+# end systemd config
+
+# start and enable relevant services
+systemctl start httpd
+systemctl enable httpd
+systemctl start mongod.service
+systemctl enable mongod.service
+systemctl start rocketchat.service
+systemctl enable rocketchat.service
 cd ../..
 export PORT=3000
 export ROOT_URL=http://192.168.100.239:3000/
